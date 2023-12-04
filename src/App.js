ac324ef647b105194815fec93ab86bb5ded7ad22
OguzKabasakal <oguzkabasakal@gmail.com>
2023-12-04 17:00:29 +0100

    part 3 solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import AlbumCard from "./components/AlbumCard";
+import AlbumList from "./components/AlbumList";
 import { data } from "./db";
 
 function App() {
   return (
     <div className="App">
       <h1 className="App-header">Music Gourmet</h1>
+      <h2 className="App-header">Featured</h2>
+      <AlbumList />
+      <h2 className="App-header">Results</h2>
       <ul>
         {data.map((item) => (
           <li key={item.id}>
